5906cc1e5ada257aee9b486f21ed8cad398a764f
vivekra <vivekraghunathan@digitalq.co.in>
2025-05-06 21:28:52 +0100

    Update YoutubePlayer.js

--- layouts/components/YoutubePlayer.js
+++ layouts/components/YoutubePlayer.js
@@ -1,8 +1,13 @@
+import dynamic from 'next/dynamic';
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const YoutubePlayer = ({ id, title, ...others }) => {
-  return <LiteYouTubeEmbed id={id} title={title} {...others} />;
-};
+const LiteYouTubeEmbed = dynamic(() => import('react-lite-youtube-embed'), {
+  ssr: false, // prevent import during server-side build
+});
 
+/* const YoutubePlayer = ({ id, title, ...others }) => {
+//  return <LiteYouTubeEmbed id={id} title={title} {...others} />;
+ };
+*/
 export default YoutubePlayer;
